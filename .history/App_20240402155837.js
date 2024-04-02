a81062df122ecb54103e57e16efe57c136a13cee
Raon <raonharu0822@kakao.com>
2024-04-02 16:03:19 +0900

    Chore: 초기세팅

---
+++ .history/App_20240402155837.js
@@ -0,0 +1,7 @@
+import {Provider} from 'react-redux';
+import RootScreen from './screen/RootScreen';
+import {Store} from './redux/Store';
+
+export default function App() {
+  return <Provider store={Store}>{/* <RootScreen /> */}</Provider>;
+}
