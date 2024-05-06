f5497aa85d3acb432f8d322c51b3a9cb9102d09e
전하윤 <kjklove0701@gmail.co>
2024-05-06 18:08:48 +0900

    내그룹홈_검색 - 최근검색어 검색 추가 및 삭제기능 & 돋보기 버튼 추가.

--- screen/myGroup/MyGroupSearchScreen.tsx
+++ screen/myGroup/MyGroupSearchScreen.tsx
@@ -5,6 +5,10 @@ import {
   TouchableOpacity,
   TextInput,
   BackHandler,
+  ActivityIndicator,
+  StyleSheet,
+  Keyboard,
+  Alert,
 } from 'react-native';
 import {
   NavigationProp,
@@ -22,15 +26,71 @@ import {
 import {Colors} from '../../assets/color/Colors';
 import Back_White from '../../assets/back_icon_white.png';
 import Close from '../../assets/Close.png';
+import {SearchLoading} from './style/MyGroupStyle.style';
+import Search from '../../assets/search.png';
 
-const MyGroupSearchScreen = () => {
+interface Search {
+  id: number;
+  search: string;
+}
+
+const MyGroupSearchScreen = ({user}: any) => {
   const navigation: NavigationProp<ParamListBase> = useNavigation();
+
+  const [text, setText] = useState('');
+
+  // const [searchList, setSearchList] = useState([
+  //   {id: 1, search: '농구'},
+  //   {id: 2, search: '스노우보드'},
+  // ]);
+
+  const [searchList, setSearchList] = useState<Search[]>([
+    {id: 1, search: '농구'},
+    {id: 2, search: '스노우보드'},
+  ]);
+
+  const onInsert = (txt: string) => {
+    //새로 등록할 항목의 id를 구함.
+    //등록된 항목 중에서 가장 큰 id를 구하고, 그 값에 1을 더함.
+    //만약 리스트가 비었다면 1을 id로 사용.
+
+    const nextId =
+      searchList.length > 0
+        ? Math.max(...searchList.map(searchList => searchList.id)) + 1
+        : 1;
+
+    const search = {
+      id: nextId,
+      search: txt,
+    };
+    setSearchList(searchList.concat(search));
+  };
+
+  const onRemove = (id: any) => {
+    const nextSearchs = searchList.filter(search => search.id !== id);
+    setSearchList(nextSearchs);
+  };
+
+  const onPress = () => {
+    onInsert(text);
+    setText('');
+    Keyboard.dismiss();
+  };
+
   const onBackPress = () => {
     if (navigation?.canGoBack()) {
       navigation.goBack();
       return true;
     }
     return false;
+  };
+
+  const onAllRemove = () => {
+    console.log(typeof searchList);
+    // setSearchList(
+    //   Object.keys(searchList).forEach(key => delete searchList[key]),
+    // );
+    // const allRemoveSearch:searchList
   };
 
   useEffect(() => {
@@ -54,24 +114,21 @@ const MyGroupSearchScreen = () => {
                 marginRight={20}
               />
             </TouchableOpacity>
-            <TextInput
-              placeholder="검색어를 입력하세요."
-              placeholderTextColor={'#9e9e9e'}
-              style={{
-                height: 48,
-                marginHorizontal: 20,
-                paddingHorizontal: 20,
-                backgroundColor: '#303030',
-                width: '90%',
-                borderTopLeftRadius: 25,
-                borderTopRightRadius: 25,
-                borderBottomLeftRadius: 25,
-                borderBottomRightRadius: 25,
-                fontSize: 14,
-                alignItems: 'center',
-                color: '#ffffff',
-              }}
-            />
+            <View style={styles.block}>
+              <TextInput
+                placeholder="검색어를 입력하세요."
+                placeholderTextColor={'#9e9e9e'}
+                style={styles.input}
+                value={text}
+                onChangeText={setText}
+                onSubmitEditing={onPress}
+              />
+              <TouchableOpacity onPress={onPress}>
+                <View style={styles.buttonStyle}>
+                  <CommonImage source={Search} width={24} height={24} />
+                </View>
+              </TouchableOpacity>
+            </View>
           </RowBox>
         </View>
         <Container
@@ -91,14 +148,14 @@ const MyGroupSearchScreen = () => {
             <CommonText fontSize={18} color={'#1C1C1C'}>
               최근 검색어
             </CommonText>
-            <TouchableOpacity>
+            <TouchableOpacity onPress={onAllRemove}>
               <CommonText fontSize={12} color={'#96A0B5'}>
                 전체 지우기
               </CommonText>
             </TouchableOpacity>
           </View>
           <FlatList
-            data={[{search: '농구'}, {search: '스노우보드'}]}
+            data={searchList}
             style={{flexGrow: 0}}
             renderItem={items => {
               return (
@@ -112,12 +169,13 @@ const MyGroupSearchScreen = () => {
                   <CommonText fontSize={14} color={'#1C1C1C'} paddingRight={10}>
                     {items.item.search}
                   </CommonText>
-                  <TouchableOpacity>
+                  <TouchableOpacity onPress={() => onRemove(items.item.id)}>
                     <CommonImage source={Close} width={10} height={10} />
                   </TouchableOpacity>
                 </View>
               );
             }}
+            keyExtractor={items => items.id.toString()}
           />
           <View style={{width: '100%', paddingVertical: 20}}>
             <View
@@ -208,5 +266,31 @@ const MyGroupSearchScreen = () => {
     </BaseSafeView>
   );
 };
+
+const styles = StyleSheet.create({
+  block: {
+    height: 48,
+    marginHorizontal: 20,
+    paddingHorizontal: 20,
+    backgroundColor: '#303030',
+    width: '90%',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  input: {
+    flex: 1,
+    fontSize: 14,
+    color: '#ffffff',
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 10,
+  },
+});
 
 export default MyGroupSearchScreen;
